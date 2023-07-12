36368b3b62b1c8977fef03cb7e404956ff5489d3
megumi-matsukura <matsukuramegumi95@gmail.com>
2023-07-12 12:55:35 +0900

    PHP課題５再提出

--- html/htdocs/kadai_005/score.php
+++ html/htdocs/kadai_005/score.php
@@ -18,7 +18,9 @@
     $score9 = 30;
     $score10 = 60;
 
-    echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10)/10;
+    $sum = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+    echo $sum/10;
     ?>    
 </body>
 
