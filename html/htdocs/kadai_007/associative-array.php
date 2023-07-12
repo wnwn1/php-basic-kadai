da90e4ae39db952e6343b3c7f84e3ca84cc644f3
megumi-matsukura <matsukuramegumi95@gmail.com>
2023-07-12 18:47:29 +0900

    PHP課題7提出

---
+++ html/htdocs/kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP課題7</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $sale_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
+        //連想配列の値を出力する
+        print_r($sale_data);
+        ?>
+    </p>    
+</body>
+</html>
